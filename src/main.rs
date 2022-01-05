498a276e3de92b611bc644006e6eec0b7e364057
Tim Jentzsch <tim-jentzsch@gmx.de>
2022-01-05 23:31:26 +0100

    Add minimal reproducable example for clippy map suggestion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[allow(dead_code)]
+fn foo(bar: Option<u8>) -> Option<u8> {
+    if let Some(val) = bar {
+        Some(val)
+    } else {
+        None
+    }
+}
